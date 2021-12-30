b0dc96de8deb2130b36d5fb4d3953180222aed78
lmuther8 <lmutes8@gmail.com>
2021-12-30 16:44:05 -0500

    Sweet

--- public/master.js
+++ public/master.js
@@ -3,12 +3,15 @@
 
 
 const header = document.getElementById('header')
+const backtotop = document.getElementById('backtotop')
 window.addEventListener('scroll', function(event) {
   var top  = window.pageYOffset || document.documentElement.scrollTop;
   if (top>=20) {
     header.classList.add('header-scrolled')
+    backtotop.classList.add('active')
   }
   else {
     header.classList.remove('header-scrolled')
+    backtotop.classList.remove('active')
   }
 })
