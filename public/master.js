c232db9e2f4eb5f436e62e177645b441f1a02d76
lmuther8 <lmutes8@gmail.com>
2022-01-02 22:27:15 -0500

    beginnings of contact form

--- public/master.js
+++ public/master.js
@@ -1,7 +1,7 @@
 // JS file for my personal website
 // Luke Muther, Dec 2021
 
-
+//scroll navbar
 const header = document.getElementById('header')
 const backtotop = document.getElementById('backtotop')
 window.addEventListener('scroll', function(event) {
@@ -15,3 +15,28 @@ window.addEventListener('scroll', function(event) {
     backtotop.classList.remove('active')
   }
 })
+
+//email form
+const emailForm = document.getElementById('contact-form')
+emailForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  var name = document.getElementById('name').value.trim();
+  var email = document.getElementById('email').value.trim();
+  var subj = document.getElementById('subject').value.trim();
+  var message = document.getElementById('message').value.trim();
+
+  var data = {
+    name,
+    email,
+    subj,
+    message
+  };
+  fetch('/mail', {
+    method: "POST",
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+  }).then(res => {
+    console.log("Request complete! response:", res);
+  });
+});
--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/public/" + "home.html");
+})
+
+app.post('/mail', (req, res) => {
+  console.log('Data:', req.body);
+  res.json({message: 'Message received!'})
 })
 
 app.listen(port, () => {
