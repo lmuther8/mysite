1bcc56a0652f5bdaee9100c3e652e61821a88728
lmuther8 <lmutes8@gmail.com>
2022-04-27 13:57:39 -0400

    awesome

--- mail.js
+++ mail.js
@@ -1,19 +1,19 @@
-const mailgun = require("mailgun-js");
-const DOMAIN = process.env.mailGunDOMAIN;
-const mg = mailgun({apiKey: process.env.mailGunAPI, domain: DOMAIN});
+// const mailgun = require("mailgun-js");
+// const DOMAIN = process.env.mailGunDOMAIN;
+// const mg = mailgun({apiKey: process.env.mailGunAPI, domain: DOMAIN});
 
 
-function sendMail(name, subject, email, text) {
-  var data = {
-  	from: "Mailgun Sandbox <postmaster@sandbox3b4ba5531d7542c590c322f2a3dcc09e.mailgun.org>",
-  	to: "lmutes8@gmail.com",
-  	subject: subject+' from: '+email,
-  	text: name+': '+text
-  };
+// function sendMail(name, subject, email, text) {
+//   var data = {
+//   	from: "Mailgun Sandbox <postmaster@sandbox3b4ba5531d7542c590c322f2a3dcc09e.mailgun.org>",
+//   	to: "lmutes8@gmail.com",
+//   	subject: subject+' from: '+email,
+//   	text: name+': '+text
+//   };
 
-  mg.messages().send(data, function (error, body) {
-  	console.log(body);
-  });
-};
+//   mg.messages().send(data, function (error, body) {
+//   	console.log(body);
+//   });
+// };
 
-module.exports = { sendMail };
+// module.exports = { sendMail };
